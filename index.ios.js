d5da3089d3e864b410113a5a30a3911dc0f60ff7
Henry Doan <henrydoan96@gmail.com>
2016-11-04 09:56:56 -0600

    flex

--- index.ios.js
+++ index.ios.js
@@ -10,9 +10,9 @@ export default class LotsOfStyles extends Component {
   render() {
     return (
       <View style={styles.container}>
-       <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-       <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
-       <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+       <View style={{flex: 1, width: 50, height: 50, backgroundColor: 'powderblue'}} />
+       <View style={{flex: 2, width: 100, height: 100, backgroundColor: 'skyblue'}} />
+       <View style={{flex: 3, width: 150, height: 150, backgroundColor: 'steelblue'}} />
       </View>
     );
   }
